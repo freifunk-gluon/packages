065b392b34add449e8ffdc624f15a347b95dfc5d
Nils Schneider <nils.schneider@gmail.com>
2012-05-23 16:47:27 +0200

    Make wizard behave friendly when luci-admin is installed

--- luci/config-mode/files/usr/lib/lua/luci/controller/ffhl/index.lua
+++ luci/config-mode/files/usr/lib/lua/luci/controller/ffhl/index.lua
@@ -1,12 +1,14 @@
 module("luci.controller.ffhl.index", package.seeall)
 
 function index()
-	entry({}, call("maybe_wizard"), "luebeck.freifunk.net", 20).dependent=false
+  local uci = luci.model.uci.cursor()
+
+  if uci:get_first("ffhl", "wizard", "enabled") == "1" then
+    local root = node()
+    if not root.target then
+      root.target = alias("wizard", "welcome")
+      root.index = true
+    end
+  end
 end
 
-function maybe_wizard()
-	-- This function should figure out whether the wizard already ran succesfully
-	-- and redirect to the wizard or the status page.
-	
-	luci.http.redirect(luci.dispatcher.build_url("wizard", "welcome"))
-end
