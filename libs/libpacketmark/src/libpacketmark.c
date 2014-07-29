59ba39ae960714b31ef2c5aa4711ff21b5497c1f
Matthias Schiffer <mschiffer@universe-factory.net>
2014-07-29 04:31:29 +0200

    Add new package libpacketmark, a LD_PRELOAD library to manipulate packet marks

---
+++ libs/libpacketmark/src/libpacketmark.c
@@ -0,0 +1,60 @@
+/*
+  Copyright (c) 2014, Matthias Schiffer <mschiffer@universe-factory.net>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+static int mark;
+static int (*socket_real)(int domain, int type, int protocol);
+
+
+void _init(void) {
+	const char *str = getenv("LIBPACKETMARK_MARK");
+	if (str)
+		mark = atoi(str);
+
+	socket_real = dlsym(RTLD_NEXT, "socket");
+}
+
+
+int socket(int domain, int type, int protocol) {
+	int fd = socket_real(domain, type, protocol);
+
+	if (fd >= 0) {
+		int errno_save = errno;
+		setsockopt(fd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark));
+		errno = errno_save;
+	}
+
+	return fd;
+}
