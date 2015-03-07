34f0ac0bb22fa303c25f7138e5880aa4865eb8dc
Matthias Schiffer <mschiffer@universe-factory.net>
2015-03-07 15:34:44 +0100

    lua-platform-info: add support for the x86-kvm_guest subtarget

---
+++ libs/lua-platform-info/files/x86/kvm_guest/platform_info.lua
@@ -0,0 +1,37 @@
+local model
+
+for line in io.lines('/proc/cpuinfo') do
+  model = line:match('^model name%s*:%s*(.+)$')
+  if model then
+    break
+  end
+end
+
+
+module 'platform_info'
+
+
+-- The OpenWrt target
+function get_target()
+   return 'x86'
+end
+
+-- The OpenWrt subtarget or nil
+function get_subtarget()
+   return 'kvm_guest'
+end
+
+-- The board name
+function get_board_name()
+   return nil
+end
+
+-- The model name
+function get_model()
+   return model
+end
+
+-- The image name for sysupgrades
+function get_image_name()
+   return nil
+end
