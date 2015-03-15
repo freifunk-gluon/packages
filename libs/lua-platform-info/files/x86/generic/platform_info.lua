d9b982d5df1098e0c42ec87e03065bcb7a46bcbf
Matthias Schiffer <mschiffer@universe-factory.net>
2015-03-15 19:39:35 +0100

    lua-platform-info: x86-generic: add image type detection

--- libs/lua-platform-info/files/x86/generic/platform_info.lua
+++ libs/lua-platform-info/files/x86/generic/platform_info.lua
@@ -8,30 +8,47 @@ for line in io.lines('/proc/cpuinfo') do
 end
 
 
+local image_name = 'x86-generic'
+
+local f = io.open('/sys/class/dmi/id/sys_vendor')
+if f then
+  local vendor = f:read('*line')
+  f:close()
+
+  if vendor then
+    if vendor:match('^VMware') or vendor:match('^VMW') then
+      image_name = 'x86-vmware'
+    elseif vendor:match('^innotek GmbH') then
+      image_name = 'x86-virtualbox'
+    end
+  end
+end
+
+
 module 'platform_info'
 
 
 -- The OpenWrt target
 function get_target()
-   return 'x86'
+  return 'x86'
 end
 
 -- The OpenWrt subtarget or nil
 function get_subtarget()
-   return 'generic'
+  return 'generic'
 end
 
 -- The board name
 function get_board_name()
-   return nil
+  return nil
 end
 
 -- The model name
 function get_model()
-   return model
+  return model
 end
 
 -- The image name for sysupgrades
 function get_image_name()
-   return nil
+  return image_name
 end
