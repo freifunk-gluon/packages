5c6476ea5893b187aa7aba1063323f38712f3455
Tobias Schramm <tobleminer@gmail.com>
2018-02-20 12:06:44 +0100

    autoupdater: use safe allocation functions
    Give the user a better error message when allocations fail by using our
    checked allocation functions.
    
    Signed-off-by: Tobias Schramm <tobleminer@gmail.com>
    [Matthias Schiffer: squash, use safe_realloc]

--- admin/autoupdater/src/manifest.c
+++ admin/autoupdater/src/manifest.c
@@ -23,9 +23,9 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 #include "hexutil.h"
 #include "manifest.h"
+#include "util.h"
 
 #include <errno.h>
 #include <limits.h>
@@ -80,14 +80,15 @@ static bool parse_rfc3339(const char *input, time_t *date) {
 
 void parse_line(char *line, struct manifest *m, const char *branch, const char *image_name) {
 	if (m->sep_found) {
-		ecdsa_signature_t *sig = malloc(sizeof(ecdsa_signature_t));
+		ecdsa_signature_t *sig = safe_malloc(sizeof(ecdsa_signature_t));
+
 		if (!parsehex(sig, line, sizeof(*sig))) {
 			free(sig);
 			fprintf(stderr, "autoupdater: warning: garbage in signature area: %s\n", line);
 			return;
 		}
 		m->n_signatures++;
-		m->signatures = realloc(m->signatures, m->n_signatures * sizeof(ecdsa_signature_t *));
+		m->signatures = safe_realloc(m->signatures, m->n_signatures * sizeof(ecdsa_signature_t *));
 		m->signatures[m->n_signatures - 1] = sig;
 	} else if (strcmp(line, "---") == 0) {
 		m->sep_found = true;
