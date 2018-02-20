0b61fee98e854f5df61b18789cdb94728b4414b5
Tobias Schramm <tobleminer@gmail.com>
2018-02-20 12:15:42 +0100

    autoupdater: Check if allocation of uci contect was successfull
    Previously the return value of uci_alloc_context was not checked leading
    to a possible null ptr dereference
    
    Signed-off-by: Tobias Schramm <tobleminer@gmail.com>
    [Matthias Schiffer: use abort()]

--- admin/autoupdater/src/settings.c
+++ admin/autoupdater/src/settings.c
@@ -109,6 +109,11 @@ static const char ** load_string_list(struct uci_context *ctx, struct uci_sectio
 
 void load_settings(struct settings *settings) {
 	struct uci_context *ctx = uci_alloc_context();
+	if (!ctx) {
+		fprintf(stderr, "autoupdater: error: failed to allocate UCI context\n");
+		abort();
+	}
+
 	ctx->flags &= ~UCI_FLAG_STRICT;
 
 	struct uci_package *p;
